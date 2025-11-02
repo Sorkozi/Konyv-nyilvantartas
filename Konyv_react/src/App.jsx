9b7887390ec9f9c4cc4ccf01708abd39f8bac0f0
Sorkozi <sarkozi.levente2005@gmail.com>
2025-11-02 13:03:44 +0100

    app.jsx kész

--- Konyv_react/src/App.jsx
+++ Konyv_react/src/App.jsx
@@ -2,34 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [konyvek, setKonyvek] = useState([]);
+  useEffect(() => {
+    axios.get("/api/konyvek").then(resp => setKonyvek(resp.data));
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h2>Könyvek listája</h2>
+      <div className="row">
+        {konyvek.map(konyv => (
+          <div key={konyv.konyvid} className="col-md-4 col-sm-6 mb-3">
+            <div className="card">
+              <div className="card-body">
+                <h5>{konyv.cim}</h5>
+                <p>Szerző: {konyv.szerzo.nev}</p>
+                <p>Kategória: {konyv.kategoria.nev}</p>
+                <p>Elérhető: {konyv.elerheto ? "Igen" : "Nem"}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
